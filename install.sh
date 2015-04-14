bfb15a697d91bd1f9dce37d9ab6dd89bb2842ad1
Rodrigo Alvares <cooltrick@gmail.com>
2015-04-14 11:40:46 -0300

    ssh directory

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ mkdir /opt/git2docker
 curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/gitreceive -o /opt/git2docker/gitreceive
 curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/git2docker-ssh -o /opt/git2docker/git2docker-ssh
 curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/git2docker -o /opt/git2docker/git2docker
-curl https://github.com/cooltrick/git2docker.io/raw/master/receiver -o /opt/git2docker/receiver
+curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/receiver -o /opt/git2docker/receiver
 ln -s /opt/git2docker/git2docker /usr/bin/git2docker
 chmod +x /opt/git2docker/*
