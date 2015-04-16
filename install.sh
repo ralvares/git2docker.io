9a2c067a9260348ad644ab7650106a962d6f5a6f
Rodrigo Alvares <cooltrick@gmail.com>
2015-04-16 11:04:25 -0300

    Client cli

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/gitreceive
 curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/git2docker-ssh -o /opt/git2docker/git2docker-ssh
 curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/git2docker -o /opt/git2docker/git2docker
 curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/receiver -o /opt/git2docker/receiver
+curl https://raw.githubusercontent.com/cooltrick/git2docker.io/master/git2docker-cli -o /opt/git2docker/git2docker-cli
 ln -s /opt/git2docker/git2docker /usr/bin/git2docker
 chmod +x /opt/git2docker/*
